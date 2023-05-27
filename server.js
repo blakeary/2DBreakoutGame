38c83c282a20bc7b5ce6dd1a238d5c15504e254f
Blake Ary <blake.ary@gmail.com>
2023-05-26 23:36:29 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', { root: __dirname });
+});
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
